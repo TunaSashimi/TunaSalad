6be90f30b35739a52f2fb854d36a3752a131ba22
jinsheng <jinsheng@didichuxing.com>
2021-06-15 22:15:20 +0800

    feat TunaSalad

--- app/src/main/java/com/tunasalad/MainActivity.java
+++ app/src/main/java/com/tunasalad/MainActivity.java
@@ -1,10 +1,8 @@
 package com.tunasalad;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
